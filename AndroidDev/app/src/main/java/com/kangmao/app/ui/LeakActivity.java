8f0178f52595fea3bee2a0bc3049e06c43a7403c
wangkangmao <1240413544@qq.com>
2016-06-26 10:19:33 +0800

    添加内存分析功能

---
+++ AndroidDev/app/src/main/java/com/kangmao/app/ui/LeakActivity.java
@@ -0,0 +1,17 @@
+package com.kangmao.app.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.kangmao.app.R;
+import com.kangmao.app.test.ActivityMgr;
+
+public class LeakActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leak);
+        ActivityMgr.getInstance().addActivity(this);
+    }
+}
