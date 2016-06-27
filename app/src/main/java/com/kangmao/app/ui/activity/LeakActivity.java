debd2abaea747bdfc2d05c42726bd45c13f8bf0e
wangkangmao <1240413544@qq.com>
2016-06-27 19:08:16 +0800

    添加NavitationLayout

--- app/src/main/java/com/kangmao/app/ui/activity/LeakActivity.java
+++ app/src/main/java/com/kangmao/app/ui/activity/LeakActivity.java
@@ -1,4 +1,4 @@
-package com.kangmao.app.ui;
+package com.kangmao.app.ui.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
---
+++ app/src/main/java/com/kangmao/app/ui/fragment/ChildFragment.java
@@ -0,0 +1,23 @@
+package com.kangmao.app.ui.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kangmao.app.R;
+
+/**
+ * Created by ywl on 2016/6/27.
+ */
+public class ChildFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+}
